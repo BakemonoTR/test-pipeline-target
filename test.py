b799e354fea162fc5e39bb23184c16eb5d5c8e51
BakemonoTR <153561325+BakemonoTR@users.noreply.github.com>
2026-03-21 14:16:19 +0000

    abc

---
+++ test.py
@@ -0,0 +1,19 @@
+import subprocess
+
+API_KEY = "sk-1234567890abcdef"
+password = "supersecret123"
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return db.execute(query)
+
+def find_duplicates(items):
+    duplicates = []
+    for i in items:
+        for j in items:
+            if i == j:
+                duplicates.append(i)
+    return duplicates
+
+def a(x, y):
+    return x+y
