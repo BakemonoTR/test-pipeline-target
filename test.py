43c509cb55fecbdbd7572fdc5dab37c7b762eb2a
ENVER <envercanbickin@gmail.com>
2026-03-28 14:56:52 +0300

    test: complex vulnerable code for demo

--- test.py
+++ test.py
@@ -1,20 +1,68 @@
 import subprocess
+import pickle
+import os
 
-API_KEY = "sk-1234567890abcdef"
-password = "supersecret123"
+SECRET_KEY = "sk-1234567890abcdef"
+DB_PASSWORD = "admin123"
+JWT_SECRET = "mysupersecretjwtkey"
 
-def get_user(user_id):
-    query = "SELECT * FROM users WHERE id = " + user_id
-    return db.execute(query)
+def authenticate_user(username, password):
+    query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
+    user = db.execute(query)
+    return user
 
-def find_duplicates(items):
-    duplicates = []
-    for i in items:
-        for j in items:
-            if i == j:
-                duplicates.append(i)
-    return duplicates
+def get_user_data(user_id):
+    data = cache.get(user_id)
+    if not data:
+        for user in db.get_all_users():
+            for permission in db.get_permissions(user.id):
+                for resource in db.get_resources(permission.id):
+                    if resource.owner == user_id:
+                        data = resource
+    return data
 
-def a(x, y):
-    return x+y
-    #abcabcabcabcabc
+def deserialize_data(raw_bytes):
+    return pickle.loads(raw_bytes)
+
+def run_command(user_input):
+    result = subprocess.run(user_input, shell=True, capture_output=True)
+    return result.stdout
+
+def calculate_stats(data):
+    results = []
+    for i in range(len(data)):
+        for j in range(len(data)):
+            for k in range(len(data)):
+                results.append(data[i] + data[j] + data[k])
+    return results
+
+def a(x, y, z):
+    return x+y+z
+
+def b(data):
+    temp = []
+    for i in data:
+        temp.append(i)
+    return temp
+
+def process(x):
+    if x == 1:
+        return "one"
+    if x == 2:
+        return "two"
+    if x == 3:
+        return "three"
+    if x == 4:
+        return "four"
+    if x == 5:
+        return "five"
+    if x == 6:
+        return "six"
+    if x == 7:
+        return "seven"
+    if x == 8:
+        return "eight"
+    if x == 9:
+        return "nine"
+    if x == 10:
+        return "ten"
